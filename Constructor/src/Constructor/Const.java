f63b53415880a17148fa35295e4d887ebed778e2
vshari <vsharichandana@gmail.com>
2017-01-02 12:23:17 +0530

    Construtors
    implementation and executio are done with the code

---
+++ Constructor/src/Constructor/Const.java
@@ -0,0 +1,35 @@
+package Constructor;
+
+public class Const {
+
+	int i;
+	String name;
+	int age;
+	Const(int a, String b, int c)
+	{
+		i=a;
+		name = b;
+		age = c;
+
+		System.out.println(i+" "+name+" "+age);
+	}
+	Const(int a, String b)
+	{
+		i=a;
+		name=b;
+		System.out.println(i+" "+name);
+	}
+
+
+	public void Display()
+	{
+		// TODO Auto-generated method stub
+		
+
+		
+
+		
+	}
+
+
+}
---
+++ Constructor/src/Constructor/TestMain.java
@@ -0,0 +1,14 @@
+package Constructor;
+import Constructor.TestMain;
+public class TestMain {
+	public static void main(String args[])
+	{
+		Const obj = new Const(100,"hari");
+		Const obj1 = new Const(101,"Chandana",22);
+
+		obj.Display();
+		obj1.Display();
+	}
+
+
+}
